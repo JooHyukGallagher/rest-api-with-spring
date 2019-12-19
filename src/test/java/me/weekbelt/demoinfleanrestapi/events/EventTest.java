f0ed4f306d7c5f13b51b371267e24346ebdb2e75
joohyuk <vfrvfr4207@gmail.com>
2019-12-19 18:18:46 +0900

    Add create events test

--- src/test/java/me/weekbelt/demoinfleanrestapi/events/EventTest.java
+++ src/test/java/me/weekbelt/demoinfleanrestapi/events/EventTest.java
@@ -13,8 +13,6 @@ public class EventTest {
                 .name("Inflearn Spring REST API")
                 .description("REST API develoment with Spring")
                 .build();
-        //when
-
         //then
         assertThat(event).isNotNull();
     }
