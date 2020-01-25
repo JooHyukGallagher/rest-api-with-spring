69a326006b72ba6d71f351e73dafd75f15723273
joohyuk <vfrvfr4207@gmail.com>
2020-01-25 21:24:30 +0900

    비즈니스 로직 적용 & 매개변수를 이용한 테스트

--- src/test/java/me/weekbelt/demoinfleanrestapi/events/EventTest.java
+++ src/test/java/me/weekbelt/demoinfleanrestapi/events/EventTest.java
@@ -1,9 +1,13 @@
 package me.weekbelt.demoinfleanrestapi.events;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+//@RunWith(JUnitParamsRunner.class)
 public class EventTest {
     
     @Test
@@ -32,5 +36,56 @@ public class EventTest {
         assertThat(event.getName()).isEqualTo(name);
         assertThat(event.getDescription()).isEqualTo(spring);
 
+    }
+
+    @DisplayName("basePrice와 maxPrice에 따른 스터디 무료 여부")
+    @ParameterizedTest(name = "{index} {displayName}")
+    @CsvSource({
+            "0, 0, true",
+            "100, 0, false",
+            "0, 100, false"
+    })
+    public void testFree(Integer basePrice, Integer maxPrice, Boolean isFree) {
+        // Given
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+
+        // When
+        event.update();
+
+        // Then
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
+
+//    JUnit4에서 파라미터값을 Type Safe하기위한 로직
+//    private Object[] parametersForTestFree() {
+//        return new Object[] {
+//          new Object[] {0, 0, true},
+//          new Object[] {100, 0, false},
+//          new Object[] {0, 100, false},
+//          new Object[] {100, 200, false}
+//        };
+//    }
+    
+    @DisplayName("스터디 오프라인 or 온라인 여부")
+    @ParameterizedTest(name = "{index} {displayName}")
+    @CsvSource({
+            "강남역 네이버 D2 스타트업 팩토리, true",
+            " , false",
+            ", false"
+    })
+    public void testOffline(String location, boolean isOffline) throws Exception {
+        //given
+        Event event = Event.builder()
+                .location(location)
+                .build();
+
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isOffline()).isEqualTo(isOffline);
     }
 }
